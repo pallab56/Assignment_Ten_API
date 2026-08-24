e0276d9a9237c0582061fd39735c0b027fa3b3b5
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-08-25 01:59:02 +0600

    product fetch from api finished

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -5,17 +5,16 @@ import 'package:assignment10/utils/api_url.dart';
 import 'package:http/http.dart' as http;
 
 class ProductController {
-  List<ProductModel> products = [];
+  List<Data> products = [];
   Future<void> getProducts() async {
     try {
       final response = await http.get(Uri.parse(ApiUrl.readProductUrl));
       print(response);
       if (response.statusCode == 200) {
-        final data = jsonDecode(response.body) as List;
-        products = data
-            .map<ProductModel>((e) => ProductModel.fromJson(e))
-            .toList();
-        print(data);
+        final jsonResponse = jsonDecode(response.body);
+        final model = ProductModel.fromJson(jsonResponse);
+        products = model.data ?? [];
+        print(products);
       } else {
         throw 'Failed to load Data ${response.statusCode}';
       }
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,34 +1,52 @@
+import 'package:assignment10/controller/product_controller.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
-  const new({super.key});
+  const HomeScreen({super.key});
 
   @override
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  ProductController controller = ProductController();
+
+  bool isLoading = true;
+  @override
+  void initState() {
+    super.initState();
+    loadData();
+  }
+
+  void loadData() async {
+    await controller.getProducts();
+    setState(() {
+      isLoading = false;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
         scrolledUnderElevation: 0,
-        title: Text('CRUD_API')),
-      body: Column(
+        title: Text('CRUD_API'),
+      ),
+      body:isLoading?Center(child: CircularProgressIndicator(strokeWidth: 3,),) :Column(
         mainAxisSize: MainAxisSize.min,
         children: [
           Expanded(
             child: GridView.builder(
-              itemCount: 10,
+              itemCount: controller.products.length,
               gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
-                childAspectRatio: .8,
+                childAspectRatio: .7,
                 crossAxisSpacing: 8,
               ),
               itemBuilder: ((context, index) {
+                final product = controller.products[index];
                 return Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 12),
                   child: Column(
@@ -41,13 +59,15 @@ class _HomeScreenState extends State<HomeScreen> {
                           borderRadius: BorderRadius.circular(15),
                         ),
                         child: Image.network(
-                          'https://images.unsplash.com/photo-1598327105666-5b89351aff97?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bW9iaWxlJTIwcGhvbmV8ZW58MHx8MHx8fDA%3D',
-                          fit: BoxFit.contain,
+                          product.img != null && product.img!.isNotEmpty?
+                          product.img.toString():'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQAvf74EGdvKeVMLO9A9WWt1QZ3htqQoh6ozMNJL9nBk87TiltBpIyUIg8M&s=10',
+                          height: 140,
+                          width: double.maxFinite,
                         ),
                       ),
-                  
+
                       Text(
-                        'Iphone pro max adfafaf af asf asf ',
+                        product.productName.toString(),
                         style: TextStyle(
                           fontSize: 24,
                           fontWeight: FontWeight.w600,
@@ -55,19 +75,29 @@ class _HomeScreenState extends State<HomeScreen> {
                         maxLines: 1,
                         overflow: TextOverflow.ellipsis,
                       ),
+                      Text(
+                        "Price : \$${product.unitPrice}",
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.w500,
+                        ),
+                      ),
                       Row(
                         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                         children: [
-                         
                           ElevatedButton.icon(
-                              onPressed: () {},
-                              icon: const Icon(Icons.edit_note),
-                              label: const Text('Edit'),
-                            ),
-                        
+                            onPressed: () {},
+                            icon: const Icon(Icons.edit_note),
+                            label: const Text('Edit'),
+                          ),
+
                           IconButton(
                             onPressed: () {},
-                            icon: Icon(Icons.delete_outline,color: Colors.red,size:28,),
+                            icon: Icon(
+                              Icons.delete_outline,
+                              color: Colors.red,
+                              size: 28,
+                            ),
                           ),
                         ],
                       ),
