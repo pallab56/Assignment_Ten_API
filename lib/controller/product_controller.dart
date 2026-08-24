7ce8c19b6b16b4c1ea54b4100f20eea8361b92bc
Pallab Chandra Das <pallab15-5698@diu.edu.bd>
2026-08-25 01:11:49 +0600

    fetch data added

--- lib/controller/product_controller.dart
+++ lib/controller/product_controller.dart
@@ -1,8 +1,26 @@
+import 'dart:convert';
+
+import 'package:assignment10/model/product_model.dart';
+import 'package:assignment10/utils/api_url.dart';
+import 'package:http/http.dart' as http;
+
 class ProductController {
-
-
-
+  List<ProductModel> products = [];
   Future<void> getProducts() async {
-    
+    try {
+      final response = await http.get(Uri.parse(ApiUrl.readProductUrl));
+      print(response);
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body) as List;
+        products = data
+            .map<ProductModel>((e) => ProductModel.fromJson(e))
+            .toList();
+        print(data);
+      } else {
+        throw 'Failed to load Data ${response.statusCode}';
+      }
+    } catch (e) {
+      throw e.toString();
+    }
   }
 }
